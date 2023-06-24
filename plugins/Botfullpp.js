aad5fc87ca2b941d09614d89db5ed6252c3a4c80
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-06-24 14:50:24 +0530

    Create Botfullpp.js

---
+++ plugins/Botfullpp.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+import jimp from 'jimp';
+
+let handler = async (m { conn command usedPrefix }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.GetShizoMediaType || '';
+  m.reply('Please wait...'); 
+  if (/image/g.test(mime) && !/webp/g.test(mime)) {
+    try {
+      let GetShizoMedia = await q.download();
+      let shizoNo = await conn.user.jid;
+      let { img } = await pepe(GetShizoMedia);
+      await conn.query({
+        tag: 'iq        attrs: {
+          to: shizoNo          type:'set          xmlns: 'w:profile:picture'
+               content: [
+          {
+            tag: 'picture            attrs: { type: 'image'            content: img
+          }
+        ]
+      });
+      m.reply(`Successfully replaced PP Bot`);
+    } catch (e) {
+      console.log(e);
+      m.reply(`Error: ${e.message}`); // changed error message to include actual error message
+    }
+  } else {
+    m.reply(`Bugged`);
+  }
+};
+
+// Set handler properties
+handler.help = ['setppfull'];
+handler.tags = ['owner'];
+handler.command = /^setppfull$/i;
+
+// Export the handler
+export default handler;
