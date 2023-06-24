9f21e400877f8a64e9e08fd1c4fc3353ae400436
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-06-24 16:51:07 +0530

    Update maker-logo2.js

--- plugins/maker-logo2.js
+++ plugins/maker-logo2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-	let response = args.join(' ').split('|')
+let response = args.join(' ').split('|')	
    let tee = `✳️ Enter a short text\n\n📌 Example  : *${usedPrefix + command}* GURU`
    let too = `✳️ Separate the text with a *+* \n\n📌 Example : \n*${usedPrefix + command}* GURU *+* BOT`
     m.react(rwait)
@@ -8,27 +8,27 @@ switch (type) {
 	
 	case 'gfx1':
 	if (!text) throw tee 
-	let chut = global.API('logo', '/api/canvas/kaneki?name=${response[0]}&apikey=${shizokeys}')
-	conn.sendFile(m.chat, chut, 'logo.png', `✅ Result`, m)
+	  let res = `https://oni-chan.my.id/api/canvas/kaneki?name=${response[0]}&apikey=${shizokeys}`
+	conn.sendFile(m.chat, res, 'logo.png', `✅ Result`, m)
 	m.react(done)
 	break
 	case 'gfx2': 
 	if (!text) throw too
 	if (!text.includes('+')) throw too  
 	let [a, b] = text.split`+`   
-	let loda = global.API('logo', '/api/girlneko', { text: a, text2: b}, 'apikey')
+	  let loda = `https://oni-chan.my.id/api/canvas/nekogirl1?text1=${response[0]}&text2=${response[2]}&apikey=${shizokeys}`
 	conn.sendFile(m.chat, loda, 'logo.png', `✅ Result`, m)
 	m.react(done)
 	break 
 	case 'gfx3':
 	if (!text) throw tee 
-	let cp = global.API('logo', '/api/rem', { text }, 'apikey')
+		  let cp = `https://oni-chan.my.id/api/canvas/gura?name=${response[0]}&apikey=${shizokeys}`
 	conn.sendFile(m.chat, cp, 'logo.png', `✅ Result`, m)
 	m.react(done)
 	break 	
         case 'gfx4': 
    if (!text) throw tee
-   let gandu = global.API('logo', '/api/sadboy', { text: 'GURU', text2: text}, 'apikey')
+		  let gandu = `https://oni-chan.my.id/api/canvas/nekosad?text1=syntax&text2=${response[0]}&apikey=${shizokeys}`
 	conn.sendFile(m.chat, gandu, 'logo.png', `✅ Result`, m)
 	m.react(done)
 	break 
@@ -36,9 +36,8 @@ switch (type) {
 } 
 } 
 handler.help = ['gfx1', 'gfx2', 'gfx3', 'gfx4']
-handler.tags = ['maker']
+handler.tags = ['vip']
 handler.command = /^(gfx1|gfx2|gfx3|gfx4)$/i
 handler.diamond = false
 
 export default handler	
-
