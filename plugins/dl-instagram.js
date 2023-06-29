40870ab1107e30c8a3e418f758b033f8e31252cc
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-06-30 00:56:54 +0530

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,18 +1,5 @@
-/*
-import axios from 'axios'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  const linknya = args[0]
 
-  if (!args[0]) throw `Input *URL*`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv`
-  let api = await axios.get(`https://xzn.wtf/api/igdl?url=${linknya}&apikey=${global.xzn}`)
-  let wm = `${global.wm}`
-  await m.reply('Sedang diproses...')
-  for (let e of api.data.media)
-    await conn.sendFile(m.chat, e, '', wm, m)
-}
-*/
-*/
+
 import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
@@ -31,9 +18,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.command = /^(ig(dl)?|instagram)$/i
-handler.limit = true
-handler.register = false
 
 export default handler
