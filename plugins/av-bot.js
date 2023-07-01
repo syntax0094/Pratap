b95b3822a2c8bd6029a53315a8187ea365deb749
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-01 12:31:34 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./src/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4", "@918010431346"])}.mp3`
+let av = `./src/mp3/${pickRandom(["@918010431346"])}.mp3`
 
 m.reply( `Hello ${taguser} Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru|@918010431346)$/i
+handler.customPrefix = /^(@918010431346)$/i
 handler.command = new RegExp
 
 export default handler
