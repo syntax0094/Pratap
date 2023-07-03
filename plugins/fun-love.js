81c413718e6854ed3d63d56c85971b0dd4fa84ea
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-04 04:20:38 +0530

    Create fun-love.js

---
+++ plugins/fun-love.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command, text }) => {
+let love = *`LOVE CALCULATOR ❤️*
+Affinity between ${text} e te: ${Math.floor(Math.random() * 100)}%
+`.trim()
+m.reply(love, null, { mentions: conn.parseMention(love) })}
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = /^(love|amore)$/i
+export default handler
