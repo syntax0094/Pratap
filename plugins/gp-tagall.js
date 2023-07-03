e2c2c2591806e2d86649a041b90cab0ffa5f29b2
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-04 02:44:50 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -5,7 +5,7 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*💌 MESSAGE:* ${pesan}`
-let teks = `*╭─「 INVOKING THE GROUP 💖🪄 」─╮*\n*│*\n*│➼*  ${oi}\n*│*\n*\n*│᯼➳ Group : *${groupMetadata.subject}`
+let teks = `*╭─「 INVOKING THE GROUP 💖🪄 」─╮*\n*│*\n*│➼*  ${oi}\n*│*\n`
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 let username = conn.getName(who)
