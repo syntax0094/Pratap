c3310de6cf8e8970a73bffa5a6782b5b7a3408f9
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-04 02:06:29 +0530

    Update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -1,74 +1,73 @@
-import { cpus as _cpus, totalmem, freemem } from 'os'
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import os from 'os'
 import util from 'util'
+import sizeFormatter from 'human-readable'
+import MessageType from '@adiwajshing/baileys'
+import fs from 'fs'
 import { performance } from 'perf_hooks'
-import { sizeFormatter } from 'human-readable'
-let format = sizeFormatter({
-  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
-  decimalPlaces: 2,
-  keepTrailingZeroes: false,
-  render: (literal, symbol) => `${literal} ${symbol}B`,
-})
-let handler = async (m, { conn, usedPrefix, command }) => {
-  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-  const used = process.memoryUsage()
-  const cpus = _cpus().map(cpu => {
-    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-    return cpu
-  })
-  const cpu = cpus.reduce((last, cpu, _, { length }) => {
-    last.total += cpu.total
-    last.speed += cpu.speed / length
-    last.times.user += cpu.times.user
-    last.times.nice += cpu.times.nice
-    last.times.sys += cpu.times.sys
-    last.times.idle += cpu.times.idle
-    last.times.irq += cpu.times.irq
-    return last
-  }, {
-    speed: 0,
-    total: 0,
-    times: {
-      user: 0,
-      nice: 0,
-      sys: 0,
-      idle: 0,
-      irq: 0
-    }
-  })
-  let old = performance.now()
-  
-  let neww = performance.now()
-  let speed = neww - old
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-let user = global.db.data.users[who]
-  
-let infobt = `
-≡ *INFO BOT*
-  
-*STATE*
-▢ *${groupsIn.length}* GROUP CHATS
-▢ *${groupsIn.length}* united groups
-▢ *${groupsIn.length - groupsIn.length}* abandoned groups
-▢ *${chats.length - groupsIn.length}* private chats
-▢ *${chats.length}* Total Chats
+let handler = async (m, { conn, usedPrefix }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const used = process.memoryUsage()
+const { restrict, antiCall, antiprivado, modejadibot } = global.db.data.settings[conn.user.jid] || {}
+const { autoread, gconly, pconly, self } = global.opts || {}
+let old = performance.now()
+let neww = performance.now()
+let speed = neww - old
+let info = `
+*📑 INFO SYNTAX MD*
 
-
- *≡ S E R V E R*
-*🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
-*🔵 FreeRAM:* ${format(freemem())}
-
-*≡  NodeJS memory *
-${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
-`
-conn.sendFile(m.chat, pp, 'prefil.jpg', infobt, m, false, { mentions: [who] })
-m.react(done)
-
+👑 *CREATOR*
+ *𝛲𝑅𝛥𝑇𝛥𝛲*
+--------------------------
+🥏 *CONTACT* 
+ *${fgig}*
+--------------------------
+🌐 *VERSION*
+ *LATEST*
+--------------------------
+💻 *PREFIx*
+ *MULTI PREFIX*
+--------------------------
+🚦 *PRIVATE CHAT*
+ *${chats.length - groups.length}*
+--------------------------
+📑 *CHATS GRUPALES*
+ *${groups.length}* 
+--------------------------
+💬 *GROUP CHAT*
+ *${chats.length}* 
+--------------------------
+⏰ *ACTIVITY*
+ *${uptime}*
+--------------------------
+👥 *USERS*
+ *${totalreg}* 
+--------------------------
+🚀 *SPEED:*
+ *${speed}*
+--------------------------
+📡 *AUTOREAD:*
+ ${autoread ? '*enabled ✅*' : '*disabled ❌*'}
+--------------------------
+🔰 *RESTRICT:*
+${restrict ? '*enabled ✅*' : '*disabled ❌*'}`.trim() 
+let aa = { quoted: m, userJid: conn.user.jid }
+let res = generateWAMessageFromContent (m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: info, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
+conn.relayMessage(m.chat, res.message, {})
 }
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['info', 'infobot', 'botinfo']
-
+handler.help = ['infobot', 'speed']
+handler.tags = ['info', 'tools']
+handler.command = /^(infobot|Infobot)$/i
 export default handler
+
+function clockString(ms) {
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
