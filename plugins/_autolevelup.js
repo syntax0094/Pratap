2822c458fa7496c0a3e431c03a7416c66e45e391
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-04 23:54:35 +0530

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,24 +1,23 @@
-//import db from '../lib/database.js'
-import { canLevelUp } from '../lib/levelling.js'
-
-export async function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    if (!user.autolevelup)
-        return !0
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier))
-        user.level++
-    user.role = global.rpg.role(user.level).name
-    if (before !== user.level) {
-        m.reply(`
-*▢ LEVEL UP*
-
- *${before}* ‣  *${user.level}*
- ROLE : *${user.role}*
- 
- _to deactivate write_
-_*/off autolevelup*_
-	`.trim())
-    }
-}
-
+import db from '../lib/database.js' 
+ import { canLevelUp } from '../lib/levelling.js' 
+  
+ export function before(m) { 
+     let user = db.data.users[m.sender] 
+     if (!user.autolevelup) 
+         return !0 
+     let before = user.level * 1 
+     while (canLevelUp(user.level, user.exp, global.multiplier)) 
+         user.level++ 
+  
+     if (before !== user.level) { 
+         user.role = global.rpg.role(user.level).name 
+         m.reply(` 
+ ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs, ${this.getName(m.sender)} ʟᴇᴠᴇʟᴇᴅ ᴜᴩ﹗ 
+ • 🏅 ᴩʀᴇᴠɪᴏᴜs ʟᴇᴠᴇʟ : ${before} 
+ • 🏅 ɴᴇᴡ ʟᴇᴠᴇʟ : ${user.level} 
+ • 🏅 ʀᴏʟᴇ : ${user.role} 
+ ᴜsᴇ *.profile* ᴛᴏ ᴄʜᴇᴄᴋ 
+         `.trim()) 
+     } 
+ } 
+ export const disabled = true
