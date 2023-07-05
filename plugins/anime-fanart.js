2d96940f5ee5105067c1a5ea22d49b693ef7b20b
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-05 20:15:43 +0530

    Update anime-fanart.js

--- plugins/anime-fanart.js
+++ plugins/anime-fanart.js
@@ -2,9 +2,10 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
 	let url = `https://api.lolhuman.xyz/api/random/art?apikey=${global.lolkey}`
-	conn.sendButton(m.chat, 'Here's the FanArt', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+	conn.sendButton(m.chat, 'Heres the FanArt', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 		
 }
+
 handler.help = ['fanart']
 handler.tags = ['anime']
 handler.command = /^(fanart)$/i
