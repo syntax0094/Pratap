40d59551fb71707956fa68a24eca40fac4c7fa07
࿐╰‿╯dͥevͣiͫlད★࿐ <114990912+Pratap0707@users.noreply.github.com>
2023-07-06 05:51:05 +0530

    Update Gitstalk.js

--- plugins/Gitstalk.js
+++ plugins/Gitstalk.js
@@ -1,59 +1,56 @@
 import axios from 'axios'
-var handler = async(m, { conn, text }) => {
+import moment from 'moment-timezone'
 
-  if (!text) return conn.reply(m.chat, '*⚠️ ENTER THE NAME OF A GITHUB USER*', m)
-  await m.reply('*B U S C A N D O ✅*')
-  let request = await githubstalk(text) 
-    let { username, following, followers, type, bio, company, blog, location, email, public_repo, public_gists, profile_pic } = request
-    let thumb = await (profile_pic)
-    let hasil = `*⬤── 「 𝙂𝙄𝙏𝙃𝙐𝘽 𝙎𝙏𝘼𝙇𝙆 」 ──⬤*
-➸ *Username*: ${username}
-➸ *Bio*: ${bio}
-➸ *Company*: ${company}
-➸ *Email:* ${email}
-➸ *Blog:* ${blog}
-➸ *public repositories:* ${public_repo}
-➸ *Public Gists:* ${public_gists}
-➸ *Follower:* ${followers}
-➸ *Following:* ${following}
-➸ *Location:* ${location}
-➸ *Type:* ${type}
-`
-
-    conn.sendFile(m.chat, avatar_url, 'github-stalk.png', teks, m)
+let handler = async (m, { conn, text }) => {
+if (!text) throw `Error!\nMasukan perintah dengan tambahan username!`
+let Quer = text.replace("https://github.com/", "").replace("@", "")
+axios.get(`https://api.github.com/users/${Quer}`)
+.then((res) =>{
+let {
+ login, 
+ type,
+ name,
+ followers, 
+ following, 
+ created_at, 
+ updated_at,
+ public_gists,
+ public_repos,
+ twitter_username,
+ bio,
+ hireable,
+ email,
+ location, 
+ blog,
+ company,
+ avatar_url,
+ html_url
+} = res.data
+var teks = `*GITHUB STALK*
+➻*User Name :* ${login}
+➻*Nick Name :* ${name}
+➻*Followers :* ${followers}
+➻*Following :* ${following}
+➻*Public Gists :* ${public_gists}
+➻*Public Repos :* ${public_repos}
+➻*Twitter :* ${twitter_username==null?'-':twitter_username}
+➻*Email :* ${email==null?'-':email}
+➻*Location :* ${location==null?'-':location}
+➻*Blog :* ${blog}
+➻*Link :* ${html_url}
+➻*Created Time :*
+  ⟣- Date : ${moment(created_at).tz('Asia/Jakarta').format('DD-MM-YYYY')}
+  ⟣- Time : ${moment(created_at).tz('Asia/Jakarta').format('HH:mm:ss')}
+➻*Updated Time :* 
+  ⟣- Date : ${moment(updated_at).tz('Asia/Jakarta').format('DD-MM-YYYY')}
+  ⟣- Time : ${moment(updated_at).tz('Asia/Jakarta').format('HH:mm:ss')}
+➻*Bio :* ${bio}`
+conn.sendFile(m.chat, avatar_url, 'github-stalk.png', teks, m)
 })
+
+}
 handler.help = ['githubstalk'].map(v => v + ' <query>')
-handler.tags = ['internet']
+handler.tags = ['stalk']
 handler.command = /^(githubstalk)$/i
 
 export default handler
-
-async function githubstalk(user) {
-    return new Promise((resolve, reject) => {
-        axios.get('https://api.github.com/users/'+user)
-        .then(({ data }) => {
-            let hasil = {
-                username: data.login,
-                nickname: data.name,
-                bio: data.bio,
-                id: data.id,
-                nodeId: data.node_id,
-                profile_pic: data.avatar_url,
-                url: data.html_url,
-                type: data.type,
-                admin: data.site_admin,
-                company: data.company,
-                blog: data.blog,
-                location: data.location,
-                email: data.email,
-                public_repo: data.public_repos,
-                public_gists: data.public_gists,
-                followers: data.followers,
-                following: data.following,
-                ceated_at: data.created_at,
-                updated_at: data.updated_at
-            }
-            resolve(hasil)
-        })
-    })
-}
