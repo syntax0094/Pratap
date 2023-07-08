e4fdb7fa7ea6095ae8c1be6b524b85610348c62a
babykababu <128393318+babykababu@users.noreply.github.com>
2023-07-09 00:31:58 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./src/mp3/${pickRandom(["Guru", "@918010431346", "Pratap"])}.mp3`
+let av = `./src/mp3/${pickRandom(["Guru", "src_mp3_@918010431346", "@918010431346", "Pratap"])}.mp3`
 
 m.reply( `*Hello ${taguser} My Boss is lost in another Multiverse, I lost contact with him...* `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
