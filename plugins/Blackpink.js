cb682f77f2d771dc47d421b43eaa9476c02f6035
babykababu <128393318+babykababu@users.noreply.github.com>
2023-07-08 22:11:43 +0530

    Update Blackpink.js

--- plugins/Blackpink.js
+++ plugins/Blackpink.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
 
     if (!img) throw img
 
-    await conn.sendFile(m.chat, img, '', 'made by Guru', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '', '*made by 𝚂𝚈𝙽𝚃𝙰𝚇*', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 
 }
 
