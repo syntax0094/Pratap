22739706fb3d5226a79525ebd0f08cd0e5807090
babykababu <128393318+babykababu@users.noreply.github.com>
2023-08-01 20:55:16 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`┊⚜️ Group : *${groupMetadata.subject}*\n┊🔰 Members : *${participants.length}*${text ? `\n┊💌 Message : ${text}\n` : ''}\n┎┅┅┅┅┅❮ 🧣*MENTIONS*🧣 ❯┅┅┅┅┅┒\n` + users.map(v => '┊❣ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ syntax ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`⚜️ Group Name : *${groupMetadata.subject}*\n🔰 Members : *${participants.length}*${text ? `\n💌 Message : ${text}\n` : ''}\n\n┎┅┅┅❮ 🧣*MENTIONS*🧣 ❯┅┅┅┒\n` + users.map(v => '┊❣ @' + v.replace(/@.+/, '')).join`\n` + '\n┖┉┉┉✪ syntax ┃ ᴮᴼᵀ ✪┉┉┉┉', null, {
         mentions: users
     })
 }
